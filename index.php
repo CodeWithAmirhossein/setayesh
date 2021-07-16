b24fea05b131b2dd8322b8dc07174b13358cda70
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-07-16 13:51:04 +0430

    fix nav links in pages

--- index.php
+++ index.php
@@ -23,13 +23,13 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href=""><i class="fa fa-newspaper"></i> اخبار</a>
+                        <a class="nav-link active" aria-current="page" href="pages/news.php"><i class="fa fa-newspaper"></i> اخبار</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#"><i class="fa fa-info"></i> درباره هنرستان</a>
+                        <a class="nav-link active" aria-current="page" href="pages/about.php"><i class="fa fa-info"></i> درباره هنرستان</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#"><i class="fa fa-phone"></i> ارتباط با هنرستان</a>
+                        <a class="nav-link active" aria-current="page" href="pages/contact.php"><i class="fa fa-phone"></i> ارتباط با هنرستان</a>
                     </li>
                 </ul>
                 <div class="navbar-nav">
