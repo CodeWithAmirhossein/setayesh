8826b2a4e3c99e89a85d89939cfaeb0c6bc9f0a4
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-07-16 14:41:20 +0430

    Add new table sql

---
+++ pack/setayesh.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE `posts`
+(
+    `row`      int(11) NOT NULL AUTO_INCREMENT,
+    `title`    text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `txt`      text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `datetime` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    PRIMARY KEY (`row`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
