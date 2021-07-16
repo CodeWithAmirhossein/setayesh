c3e8b689d4fceeb366be24a6508f628d3ee762fc
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-07-16 14:42:24 +0430

    Add news table

--- pack/setayesh.sql
+++ pack/setayesh.sql
@@ -10,3 +10,16 @@ CREATE TABLE `posts`
   AUTO_INCREMENT = 1
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS `news`;
+CREATE TABLE `news`
+(
+    `row`      int(11) NOT NULL AUTO_INCREMENT,
+    `title`    text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `news`     text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `datetime` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    PRIMARY KEY (`row`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
