5af0e40bd9df2fb04d2b5eab3fb9c18c18d7b7ba
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-07-20 11:16:12 +0430

    Get admin data and workin on it

---
+++ admin/index.php
@@ -0,0 +1,92 @@
+<?php
+session_start();
+
+$status = $_SESSION['status'];
+
+if (!$status) {
+    ?>
+    <script>
+        window.location.replace("../");
+    </script>
+    <?php
+}
+
+$id = $_SESSION['id'];
+
+$server = "localhost";
+$username = "setayesh";
+$password = "setayesh";
+$database = "setayesh";
+
+$connection = mysqli_connect($server, $username, $password, $database);
+
+$get_user_data_query = "SELECT * FROM `admin` WHERE id = '$id'";
+$get_ser_data_result = mysqli_query($connection, $get_user_data_query);
+$admin = mysqli_fetch_assoc($get_ser_data_result);
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Setayesh - About</title>
+    <link href="../pack/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="../pack/main.css" rel="stylesheet" type="text/css">
+    <script src="https://kit.fontawesome.com/4a679d8ec0.js" crossorigin="anonymous"></script>
+</head>
+<body dir="rtl">
+<div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-custom fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="../"><i class="fa fa-bank"></i> هنرستان آفرینش</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="news.php"><i class="fa fa-newspaper"></i> اخبار</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="about.php"><i class="fa fa-info"></i> درباره هنرستان</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="contact.php"><i class="fa fa-phone"></i> ارتباط با هنرستان</a>
+                    </li>
+                </ul>
+                <div class="navbar-nav">
+                    <?php
+                    if ($status) {
+                        ?>
+                        <a class="nav-link active" href="../user"><i class="fa fa-dashboard"></i> ورود به پنل</a> <a class="nav-link active" href="account/logout.php"><i class="fa fa-sign-out"></i> خروج از حساب</a>
+                        <?php
+                    }
+                    else {
+                        ?>
+                        <a class="nav-link active" href="../account"><i class="fa fa-sign-in"></i> ورود یا ساخت حساب</a>
+                        <?php
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <br>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6">
+                <h3>خوش آمدید <span><?php echo $admin['name']; ?> !</span></h3>
+                <p>!</p>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="../pack/bootstrap.js"></script>
+</body>
+</html>
